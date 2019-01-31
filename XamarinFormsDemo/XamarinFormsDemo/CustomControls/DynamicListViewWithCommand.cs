5f2a63d514a285b6d89cdc54ce3c40320916ac75
Sara Llorente <sara.llorente@cognizant.com>
2019-01-31 17:54:27 +0100

    Fixed things

--- XamarinFormsDemo/XamarinFormsDemo/CustomControls/DynamicListViewWithCommand.cs
+++ XamarinFormsDemo/XamarinFormsDemo/CustomControls/DynamicListViewWithCommand.cs
@@ -6,49 +6,21 @@ using Xamarin.Forms;
 
 namespace XamarinFormsDemo.CustomControls
 {
-    public class DynamicListViewWithCommand<T>: ListView where T : class
+    public class DynamicListViewWithCommand: ListView
     {
-        private readonly ObservableCollection<T> observableList;
-
-        /// <summary>
-        /// The full items source property
-        /// </summary>
-        public static BindableProperty FullItemsSourceProperty = BindableProperty.Create
-            (nameof(FullItemsSource), typeof(IList<T>), typeof(DynamicListViewWithCommand<T>), default(IList<T>), BindingMode.OneWay, null,
-                OnFullItemsSourceChanged);
-
         public static readonly BindableProperty GetMoreElementsCommandProperty = BindableProperty.Create(
             nameof(GetMoreElementsCommand),
             typeof(ICommand),
-            typeof(DynamicListViewWithCommand<T>),
+            typeof(DynamicListViewWithCommand),
             null,
             BindingMode.OneWay);
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DynamicListViewWithCommand{T}"/> class.
+        /// Initializes a new instance of the <see cref="DynamicListViewWithCommand"/> class.
         /// </summary>
         public DynamicListViewWithCommand()
         {
-            this.observableList = new ObservableCollection<T>();
-            this.ItemsSource = this.observableList;
             this.ItemAppearing += this.OnItemAppearing;
-            this.PropertyChanged += OnPropertyChanged;
-        }
-
-        /// <summary>
-        /// Gets or sets the full items source.
-        /// </summary>
-        /// <value>The full items source.</value>
-        public IList<T> FullItemsSource
-        {
-            get
-            {
-                return (IList<T>)this.GetValue(FullItemsSourceProperty);
-            }
-            set
-            {
-                this.SetValue(FullItemsSourceProperty, value);
-            }
         }
 
         /// <summary>
@@ -62,37 +34,6 @@ namespace XamarinFormsDemo.CustomControls
         }
 
         /// <summary>
-        /// Called when [full items source changed].
-        /// </summary>
-        /// <param name="bindableElement">The bindable element.</param>
-        /// <param name="oldvalue">The oldvalue.</param>
-        /// <param name="newvalue">The newvalue.</param>
-        private static void OnFullItemsSourceChanged(BindableObject bindableElement, object oldvalue, object newvalue)
-        {
-            var listView = bindableElement as DynamicListViewWithCommand<T>;
-            listView?.OnFullItemsSourceChanged(listView);
-        }
-
-        /// <summary>
-        /// Called when [full items source changed].
-        /// </summary>
-        /// <param name="listView">The list view.</param>
-        private void OnFullItemsSourceChanged(DynamicListViewWithCommand<T> listView)
-        {
-            if (listView.FullItemsSource != null)
-            {
-                this.observableList.Clear();
-
-                var items = listView.FullItemsSource.ToList();
-
-                foreach (var elem in items)
-                {
-                    this.observableList.Add(elem);
-                }
-            }
-        }
-
-        /// <summary>
         /// Called when [item appearing].
         /// </summary>
         /// <param name="sender">The sender.</param>
@@ -101,33 +42,29 @@ namespace XamarinFormsDemo.CustomControls
         /// </param>
         private void OnItemAppearing(object sender, ItemVisibilityEventArgs e)
         {
-            var listView = sender as DynamicListViewWithCommand<T>;
-            if (listView?.FullItemsSource == null || !listView.FullItemsSource.Any())
+            var listView = sender as DynamicListViewWithCommand;
+            if (listView?.ItemsSource == null)
             {
                 return;
             }
 
-            var indexOfAppearedItem = listView.FullItemsSource.IndexOf((T)e.Item);
-            var indexOfLatestItemInList = this.observableList.IndexOf((T)this.observableList.LastOrDefault());
-
-            if (indexOfAppearedItem == indexOfLatestItemInList)
+            var castedList = new List<object>();
+            foreach (var item in listView.ItemsSource)
             {
-                listView.GetMoreElementsCommand?.Execute(null);
-            }
-        }
-
-        private void OnPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            var listView = sender as DynamicListViewWithCommand<T>;
-            if (listView?.FullItemsSource == null || !listView.FullItemsSource.Any())
-            {
-                return;
+                castedList.Add(item as object);
             }
 
-            this.observableList.Clear();
-            foreach (var item in listView.FullItemsSource)
+            if (castedList != null)
             {
-                this.observableList.Add(item);
+                var itemApearingIndex = castedList.IndexOf(e.Item);
+                var listLastItemIndex = castedList.IndexOf(castedList.LastOrDefault());
+
+
+                if (itemApearingIndex == listLastItemIndex)
+                {
+                    listView.GetMoreElementsCommand.Execute(null);
+                }
+
             }
         }
 
