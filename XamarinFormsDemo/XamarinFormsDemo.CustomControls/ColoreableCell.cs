8d71a9f022270f4af42bc0f58c06f01ee5c2811e
Sara Llorente <sara.llorente@cognizant.com>
2019-10-06 14:08:25 +0200

    Added Custom Controls Project

---
+++ XamarinFormsDemo/XamarinFormsDemo.CustomControls/ColoreableCell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsDemo.CustomControls
+{
+    public class ColoreableCell : ViewCell
+    {
+        public ColoreableCell()
+        {
+            this.Tapped += this.ColoredCell_Tapped;
+        }
+
+        public BindableProperty CellBackgroundColorProperty = BindableProperty.Create(nameof(CellBackgroundColor), typeof(string), typeof(ColoreableCell), Color.White, BindingMode.Default);
+
+        public string CellBackgroundColor
+        {
+            get => (string)this.GetValue(this.CellBackgroundColorProperty);
+            set => this.SetValue(this.CellBackgroundColorProperty, value);
+        }
+
+        private void ColoredCell_Tapped(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.CellBackgroundColor))
+            {
+                var color = Color.FromHex(this.CellBackgroundColor);
+                this.View.BackgroundColor = color;
+            }
+        }
+    }
+}
