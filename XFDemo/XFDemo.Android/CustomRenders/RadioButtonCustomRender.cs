322866391b16088aebcafe2dcbf61bc6af2e02b2
Sara Llorente <sara.llorente@cognizant.com>
2019-10-08 10:42:49 +0200

    Created custom render to android radio button

---
+++ XFDemo/XFDemo.Android/CustomRenders/RadioButtonCustomRender.cs
@@ -0,0 +1,25 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XFDemo.CustomControls;
+using XFDemo.Droid.CustomRenders;
+
+[assembly: ExportRenderer(typeof(RadioButton), typeof(RadioButtonCustomRender))]
+namespace XFDemo.Droid.CustomRenders
+{
+    public class RadioButtonCustomRender : ImageButtonRenderer
+    {
+        public RadioButtonCustomRender(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ImageButton> e)
+        {
+            var xfControl = e.NewElement as RadioButton;
+            if (xfControl != null)
+            {
+                base.OnElementChanged(e);
+                Element.HeightRequest = xfControl.ImageHeight;
+                Element.WidthRequest = xfControl.ImageWidth;
+            }
+        }
+    }
+}
